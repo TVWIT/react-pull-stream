9c05d7f050e927565503f92942ebd83bbfb250c7
Adam <adam.ellsworth@tvw.org>
2017-10-09 16:09:52 -0700

    cleanup and finalize

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-var React = require('react');
-var createClass = require('create-react-class');
-var xtend = require('xtend');
-var S = require('pull-stream/pull');
-var Pushable = require('pull-pushable');
-var Drain = require('pull-stream/sinks/drain');
-var Abortable = require('pull-abortable');
-var Notify = require('pull-notify');
+var React = require('react')
+var createClass = require('create-react-class')
+var xtend = require('xtend')
+var S = require('pull-stream/pull')
+var Pushable = require('pull-pushable')
+var Drain = require('pull-stream/sinks/drain')
+var Abortable = require('pull-abortable')
+var Notify = require('pull-notify')
 
 function ReactStream (Elmt, onEnd) {
-    var notify = Notify();
-    var sourceNotify = Notify();
-    var pushable = Pushable();
+    var notify = Notify()
+    var sourceNotify = Notify()
+    var pushable = Pushable()
 
     var source = S(
         pushable,
         S.through(sourceNotify)
-    );
-    source.listen = sourceNotify.listen;
-    source.end = pushable.end;
-    source.push = pushable.push;
+    )
+    source.listen = sourceNotify.listen
+    source.end = pushable.end
+    source.push = pushable.push
 
-    var listener = notify.listen();
+    var listener = notify.listen()
 
     var DrainElmt = createClass({
         componentWillMount: function () {
-            var self = this;
+            var self = this
             var drain = Drain(function onEvent (ev) {
-                self.setState(ev);
-            });
-            S( listener, drain );
+                self.setState(ev)
+            })
+            S( listener, drain )
         },
 
         render: function () {
@@ -40,29 +40,29 @@ function ReactStream (Elmt, onEnd) {
                     { push: pushable.push }
                 ),
                 []
-            );
+            )
         }
-    });
+    })
 
-    var abortable = Abortable();
+    var abortable = Abortable()
     var drain = S(
         abortable,
         Drain(function onEvent (ev) {
-            notify(ev);
+            notify(ev)
         }, function _onEnd (err) {
-            if (err) throw err;
-            pushable.end(err);
-            sourceNotify.end(err);
-            if (onEnd) onEnd(err);
+            if (err) throw err
+            pushable.end(err)
+            sourceNotify.end(err)
+            if (onEnd) onEnd(err)
         })
-    );
+    )
 
     return {
         source: source,
         sink: drain,
         view: DrainElmt,
         abort: abortable.abort.bind(abortable)
-    };
+    }
 }
 
-module.exports = ReactStream;
+module.exports = ReactStream
